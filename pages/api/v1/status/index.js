05ffc986dbe895f075ab31efd0a847792def6622
Levi Lael <levilael2@hotmail.com>
2026-01-29 15:32:34 -0300

    database version test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows);
-  response.status(200).json();
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const activeConnectionsResult = await database.query(
+    "SELECT count(*)::int from pg_stat_activity",
+  );
+  const activeConnectionsValue = activeConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        active_connections: parseInt(activeConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
